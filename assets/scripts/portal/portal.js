6b628889fd4ee2e298b1c9c2c317dda26f49cd2b
Arnel T. Lenteria <arnel.lenteria@gmail.com>
2020-07-17 23:23:03 +0800

    voucher qr reader

---
+++ assets/scripts/portal/portal.js
@@ -0,0 +1,60 @@
+function initQrScanner(){
+  var qi = document.createElement('i');
+  qi.classList = "fa fa-qrcode qr-icon";
+  qi.style = 'left: 192px; position: absolute; margin-left: -172px; font-size: 28px; cursor: pointer; z-index: 9999; top: 4px;';
+  qi.id = 'qr-btn'
+  var vi = document.getElementsByName('code')[0]
+  if(!vi) return;
+  var s = document.createElement('script');
+  var e = document.createElement('div');
+  e.id = 'reader';
+  var ie = document.createElement('input')
+  ie.type = 'file';
+  ie.id = 'qr-input-file';
+  ie.accept = 'image/*';
+  ie.style = 'opacity:0;width:1px;'
+  var html5QrCode;
+  s.onload = function(){
+    document.body.appendChild(e);
+    document.body.appendChild(ie);
+    html5QrCode = new Html5Qrcode("reader");
+    vi.parentElement.appendChild(qi)
+  }
+  s.setAttribute('src','/plugins/voucher-qr-reader/assets/scripts/html5-qrcode.min.js');
+  document.body.appendChild(s);
+  qi.onclick = function(){
+    return ie.click()
+  }
+  ie.addEventListener('change', e => {
+    if (!e.target.files || !e.target.files.length) return;
+    var imageFile = e.target.files[0];
+    vi = document.getElementsByName('code')[0]
+    html5QrCode.scanFile(imageFile, false)
+    .then(function(code){
+      vi.value = code
+      vi.dispatchEvent(new Event("input"))
+      setTimeout(function(){
+        vi.form.dispatchEvent(new Event("submit"))
+      })
+    })
+    .catch(function(err){
+      alert("Voucher QR code can't be recognized!")
+      vi.value = ""
+      vi.dispatchEvent(new Event("input"))
+      console.log(`Error scanning file. Reason: ${err}`)
+    }).finally(function(){
+      ie.value = null;
+    })
+  });
+  document.addEventListener("click", function(e){
+    var is_button = e.target.tagName == "BUTTON" || e.target.classList.contains("btn")
+    if(!is_button) return;
+    setTimeout(function(){
+      var vi = document.getElementsByName('code')[0]
+      if(!vi) return;
+      if(document.getElementsByClassName('fa-qrcode qr-icon').length) return;
+      vi.parentElement.appendChild(qi)
+    }, 200)
+  })
+}
+setTimeout(initQrScanner, 3000);
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+var { app } = require('../core.js')
+
+module.exports = {
+  async init(){
+  },
+}
